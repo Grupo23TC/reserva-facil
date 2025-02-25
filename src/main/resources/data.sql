2445cdd4ec1634e635a14123e3718a26bc419494
caiotfernandes <caiotfernandes@gmail.com>
2025-02-25 20:33:52 -0300

    Correção beneficiario sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,44 @@
--- Inserindo dados na tabela 'client'
-INSERT INTO client (id, client_id, client_secret, redirect_uri, scope) VALUES ('550e8400-e29b-41d4-a716-446655440000', 'meuClient', '$2a$10$/yS0vgwkCm3h6PCMAwMA6eKMeqEuyEuU9hWjjjUjKVwjcYQBOEh.y', 'http://localhost:8080/authorized', 'read write');
+-- client
+INSERT INTO client (id, client_id, client_secret, redirect_uri, scope)
+VALUES ('550e8400-e29b-41d4-a716-446655440000', 'meuClient', '$2a$10$/yS0vgwkCm3h6PCMAwMA6eKMeqEuyEuU9hWjjjUjKVwjcYQBOEh.y', 'http://localhost:8080/authorized', 'read write');
 
--- Inserindo dados na tabela 'usuario'
--- INSERT INTO usuario (id, cns, senha, roles) VALUES ('550e8400-e29b-41d4-a716-446655440005', '123456789012345', '$2a$10$0Kxhiicu5MeLLeTFA4WosOqstc.czwhYNLqheZsQNVMseDi6xXfN.', ARRAY['PACIENTE']);
-
+-- usuario
 INSERT INTO usuario (id, cns, senha, ativo)
-VALUES
-    ('550e8400-e29b-41d4-a716-446655440005', '123456789012345',
-     '$2a$10$0Kxhiicu5MeLLeTFA4WosOqstc.czwhYNLqheZsQNVMseDi6xXfN.', false);
+VALUES ('550e8400-e29b-41d4-a716-446655440005', '123456789012345', '$2a$10$0Kxhiicu5MeLLeTFA4WosOqstc.czwhYNLqheZsQNVMseDi6xXfN.', false);
 
--- Inserindo role na tabela 'roles'
+-- role
 INSERT INTO role (id, authority)
-VALUES
-    ('550e8400-e29b-41d4-a716-446655440010', 'PACIENTE');
+VALUES ('550e8400-e29b-41d4-a716-446655440010', 'PACIENTE');
 
 -- Associando usuário à role na tabela 'usuario_role'
 INSERT INTO usuario_role (user_id, role_id)
-VALUES
-    ('550e8400-e29b-41d4-a716-446655440005', '550e8400-e29b-41d4-a716-446655440010');
+VALUES ('550e8400-e29b-41d4-a716-446655440005', '550e8400-e29b-41d4-a716-446655440010');
+
+-- endereco
+INSERT INTO endereco (id, logradouro, bairro, cidade, estado, complemento)
+VALUES ('550e8400-e29b-41d4-a716-446655440001', 'Rua das Flores', 'Centro', 'São Paulo', 'SP', 'Sala 101');
+
+-- beneficiario
+INSERT INTO beneficiario (
+    cns,
+    nome,
+    cpf,
+    telefone,
+    faixa_etaria,
+    genero,
+    tipo_medicamento,
+    ativo,
+    endereco_id,
+    usuario_id
+) VALUES (
+             '123456789012345',
+             'Fulano da Silva',
+             '01234567890',
+             '11999999999',
+             'FAIXA_0_a_18',
+             'OUTROS',
+             'ESPECIAIS',
+             true,
+             '550e8400-e29b-41d4-a716-446655440001',
+             '550e8400-e29b-41d4-a716-446655440005'
+         );
